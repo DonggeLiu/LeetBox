8463b3368542773ba78117db0226d85d101a1bfe
Alan32Liu <alan32.liu@gmail.com>
2018-06-16 22:13:38 +1000

    use inner functions

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -3,18 +3,19 @@ Question:
 https://leetcode.com/problems/product-of-array-except-self/description/
 """
 
+
 class Solution:
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
-        """    
-        return self.compute_products(znums[::-1], self.compute_products(nums, [1]*len(nums)))
-        
-    def compute_products(self, nums, ans):
-        product = 1
-        for i, num in enumerate(nums):
-            ans[i] = ans[i] * product
-            product *= num
-            
-        return ans[::-1]
+        """
+
+        def compute_products(refs, ans):
+            product = 1
+            for i, ref in enumerate(refs):
+                ans[i] = ans[i] * product
+                product *= ref
+            return ans[::-1]
+
+        return compute_products(nums[::-1], compute_products(nums, [1]*len(nums)))
