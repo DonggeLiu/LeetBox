18466450ea4e40139cc989c71af845fd0a42d36f
Alan32Liu <alan32.liu@gmail.com>
2018-06-17 20:46:47 +1000

    The second approach: using library

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -7,9 +7,19 @@ https://leetcode.com/problems/permutations/description/
 class Solution:
     def permute(self, nums):
         """
+        Implementing manually
         :type nums: List[int]
         :rtype: List[List[int]]
         """
         return [pre_pmt+[num]
                 for num in nums for pre_pmt in self.permute([x for x in nums if num != x])
                ] if len(nums) else [[]]
+    
+
+    def permute(self, nums):
+        """
+        Using library
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+    		return list(itertools.permutations(nums))
