bb248172318c4b67b6a2821ba942a64b58c8be1f
Alan32Liu <alan32.liu@gmail.com>
2021-01-01 11:52:48 +1100

    Question 313

---
+++ 313. Super Ugly Number.py
@@ -0,0 +1,14 @@
+from heapq import heappop, heappush
+
+
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        heap = [1]
+        cur = 1
+        for _ in range(n):
+            cur = heappop(heap)
+            for p in primes:
+                heappush(heap, cur * p)
+                if not cur % p:
+                    break
+        return cur
